c415addcb44c1ea5a81c982d45253c5bf2112a7b
Everett Quebral <Everett.Quebral@gmail.com>
2018-09-21 15:48:05 -0700

    Using Loadable to asyncload components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
+/* eslint-disable import/first */
+
 import React, { Fragment } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
 } from 'react-router-dom'
+import Loadable from 'react-loadable'
 
-import StarChatter from './StarChatter';
-import ChatApp from './ChatApp'
-import Main from './Main'
-import ReactiveUI from './ReactiveUI'
-import RenderProps from './RenderProps'
-import AdvancedUI from './AdvancedUI'
-import HOC from './HOC'
+const Loading = () => <div>Loading...</div>
+
+const StarChatterLoadable = Loadable({
+  loader: () => import('./StarChatter'),
+  loading: () => <Loading />
+})
+
+const ChatAppLoadable = Loadable({
+  loader: () => import('./ChatApp'),
+  loading: () => <Loading />
+})
+
+const MainLoadable = Loadable({
+  loader: () => import('./Main'),
+  loading: () => <Loading />
+})
+
+const ReactiveUILoadable = Loadable({
+  loader: () => import('./ReactiveUI'),
+  loading: () => <Loading />
+})
+
+const RenderPropsLoadable = Loadable({
+  loader: () => import('./RenderProps'),
+  loading: () => <Loading />
+})
+
+const AdvancedUILoadable = Loadable({
+  loader: () => import('./AdvancedUI'),
+  loading: () => <Loading />
+})
+
+const HOCLoadable = Loadable({
+  loader: () => import('./HOC'),
+  loading: () => <Loading />
+})
 
 // import logo from './logo.svg';
 import './App.css';
@@ -20,13 +52,13 @@ const App = () => (
   <Router>
     <Fragment>
       <Switch>
-        <Route exact path='/' component={Main} />
-        <Route exact path='/chat' component={ChatApp} />
-        <Route exact path='/star' component={StarChatter} />
-        <Route exact path='/reactiveui' component={ReactiveUI} />
-        <Route exact path='/renderprops' component={RenderProps} />
-        <Route exact path='/hoc' component={HOC} />
-        <Route exact path='/advancedui' component={AdvancedUI} />
+        <Route exact path='/' component={MainLoadable} />
+        <Route exact path='/chat' component={ChatAppLoadable} />
+        <Route exact path='/star' component={StarChatterLoadable} />
+        <Route exact path='/reactiveui' component={ReactiveUILoadable} />
+        <Route exact path='/renderprops' component={RenderPropsLoadable} />
+        <Route exact path='/hoc' component={HOCLoadable} />
+        <Route exact path='/advancedui' component={AdvancedUILoadable} />
       </Switch>
     </Fragment>
   </Router>
